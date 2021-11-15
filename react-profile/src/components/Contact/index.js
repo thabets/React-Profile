3dc33e95fd8fbdaa66e083bde2b80994da51c70a
Sam Thabet <sallam.thabet@gmail.com>
2021-11-14 19:28:12 -0500

    fixed about me section

--- react-profile/src/components/Contact/index.js
+++ react-profile/src/components/Contact/index.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-
+import { Form, Button } from "react-bootstrap";
 import { validateEmail } from "../../utils/helpers";
 
 function ContactForm() {
@@ -80,6 +80,31 @@ function ContactForm() {
           Submit
         </button>
       </form>
+
+      <Form>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control type="email" placeholder="Enter email" />
+          <Form.Text className="text-muted">
+            We'll never share your email with anyone else.
+          </Form.Text>
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control type="password" placeholder="Password" />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicCheckbox">
+          <Form.Check type="checkbox" label="Check me out" />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+          <Form.Label>Example textarea</Form.Label>
+          <Form.Control as="textarea" rows={3} />
+        </Form.Group>
+        <Button variant="primary" type="submit" onSubmit={handleSubmit}>
+          Submit
+        </Button>
+      </Form>
     </section>
   );
 }
