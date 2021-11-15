6c25bc1c4b3aec831f302dd86e0e3bf70f5b4e98
Sam Thabet <sallam.thabet@gmail.com>
2021-11-15 00:59:07 -0500

    fixed contact form

--- react-profile/src/components/Contact/index.js
+++ react-profile/src/components/Contact/index.js
@@ -42,65 +42,35 @@ function ContactForm() {
 
   return (
     <section>
-      <h1 className="center">Contact me</h1>
-      <form id="contact-form" onSubmit={handleSubmit}>
-        <div>
-          <label htmlFor="name">Name:</label>
-          <input
-            type="text"
-            name="name"
-            defaultValue={name}
+      <h1 className="center mar">Contact me</h1>
+           <Form className="mar pd">
+        <Form.Group className="mb-3">
+          <Form.Label>Name</Form.Label>
+          <Form.Control
+            type="name"
+            placeholder="First and Last"
             onBlur={handleChange}
           />
-        </div>
-        <div>
-          <label htmlFor="email">Email address:</label>
-          <input
-            type="email"
-            name="email"
-            defaultValue={email}
-            onBlur={handleChange}
-          />
-        </div>
-        <div>
-          <label htmlFor="message">Message:</label>
-          <textarea
-            name="message"
-            rows="5"
-            defaultValue={message}
-            onBlur={handleChange}
-          />
-        </div>
-        {errorMessage && (
-          <div>
-            <p className="error-text">{errorMessage}</p>
-          </div>
-        )}
-        <button data-testid="button" type="submit">
-          Submit
-        </button>
-      </form>
+        </Form.Group>
 
-      <Form>
         <Form.Group className="mb-3" controlId="formBasicEmail">
           <Form.Label>Email address</Form.Label>
-          <Form.Control type="email" placeholder="Enter email" />
+          <Form.Control
+            defaultValue={email}
+            type="email"
+            placeholder="Enter email"
+            onBlur={handleChange}
+          />
           <Form.Text className="text-muted">
             We'll never share your email with anyone else.
           </Form.Text>
         </Form.Group>
 
-        <Form.Group className="mb-3" controlId="formBasicPassword">
-          <Form.Label>Password</Form.Label>
-          <Form.Control type="password" placeholder="Password" />
-        </Form.Group>
-        <Form.Group className="mb-3" controlId="formBasicCheckbox">
-          <Form.Check type="checkbox" label="Check me out" />
-        </Form.Group>
         <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
           <Form.Label>Example textarea</Form.Label>
-          <Form.Control as="textarea" rows={3} />
+          <Form.Control type="message" rows={3} onBlur={handleChange} />
         </Form.Group>
+
         <Button variant="primary" type="submit" onSubmit={handleSubmit}>
           Submit
         </Button>
